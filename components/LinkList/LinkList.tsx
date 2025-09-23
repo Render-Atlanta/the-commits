05bf9a0fb2e50170d094518be42e61b053d1f667
Andrew Reifman <andrewr23@gmail.com>
2025-09-23 11:55:34 -0400

    fix: link list links

--- components/LinkList/LinkList.tsx
+++ components/LinkList/LinkList.tsx
@@ -21,19 +21,19 @@ export function LinkList({ className, links }: Props) {
     <div className={clsx(className, 'flex flex-col items-center justify-center text-center')}>
       {links.map(
         (
-          link: { text: string; link: { href: string; target?: '_self' | '_blank' } },
+          linkItem: { text: string; link: { href: string; target?: '_self' | '_blank' } },
           index: number
         ) => (
           <Link
             key={index}
-            href={link.link.href}
-            target={link.link.target}
+            href={linkItem.link?.href || ''}
+            target={linkItem.link?.target}
             className="group/link inline-flex items-center gap-x-4 py-1 leading-normal md:py-2"
           >
             <span className="relative z-10 translate-x-4 font-heading text-3xl font-light text-foreground transition-all duration-300 ease-out hover:text-primary group-hover/link:translate-x-0 sm:translate-x-6 md:text-4xl lg:text-5xl xl:text-6xl">
-              {link.text}
+              {linkItem.text}
             </span>
-            <div className="grid size-8 -translate-x-4 place-items-center bg-primary opacity-0 transition-all duration-300 ease-out group-hover/link:translate-x-0 group-hover/link:opacity-100 sm:-translate-x-6 md:size-10 md:translate-y-0.5 xl:size-12 xl:translate-y-1">
+            <div className="grid size-8 -translate-x-4 place-items-center bg-primary opacity-0 transition-all duration-300 ease-out group-hover/link:translate-x-0 group-hover/link:opacity-100 sm:-translate-x-6 md:size-10 xl:size-12">
               <svg viewBox="0 0 16 16" fill="none" className="size-4 stroke-background">
                 <path strokeWidth={2} d="M2 1h13v13M15 1 1 15" />
               </svg>
