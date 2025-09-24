79b67e2036f324b0119730e04ba3f25937c01c0a
Andrew Reifman <andrewr23@gmail.com>
2025-09-24 13:36:59 -0400

    feat: form styles, scroll arrow

--- components/TrophyStickyContainer/TrophyStickyContainer.tsx
+++ components/TrophyStickyContainer/TrophyStickyContainer.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { useEffect, useState } from 'react'
+
 import Spline from '@splinetool/react-spline'
 import clsx from 'clsx'
 
@@ -9,8 +11,23 @@ export interface Props {
 }
 
 export function TrophyStickyContainer({ className, sections }: Props) {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 20)
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
   return (
-    <div className={clsx(className, 'relative flex flex-col-reverse items-center sm:flex-row')}>
+    <div
+      className={clsx(
+        className,
+        'relative flex flex-col-reverse items-center justify-center sm:flex-row'
+      )}
+    >
       <div className="w-full flex-1 shrink-0 border-r border-foreground/25 sm:w-1/2">
         {sections.map((section: { children: React.ReactNode }, index: number) => (
           <div
@@ -23,6 +40,17 @@ export function TrophyStickyContainer({ className, sections }: Props) {
       </div>
       <div className="aspect-square h-96 w-full flex-1 shrink-0 sm:sticky sm:inset-y-0 sm:right-0 sm:top-0 sm:aspect-auto sm:h-screen">
         <Spline scene="https://prod.spline.design/2Gm4QkPYPfTwREcA/scene.splinecode" />
+      </div>
+
+      <div
+        className={clsx(
+          'absolute left-6 top-[calc(100vh-64px)] hidden transition-opacity duration-300 ease-linear sm:left-8 sm:block md:left-12 xl:left-20',
+          isScrolled && 'opacity-0'
+        )}
+      >
+        <svg viewBox="0 0 34 20" className="w-9 stroke-primary" fill="none">
+          <path strokeWidth={3} d="M32 2 17 17 2 2" />
+        </svg>
       </div>
     </div>
   )
